74b5bf87ce74e33f6a4ff985b23bc257444a3f62
Neil Gandhi <65824883+ntch2000@users.noreply.github.com>
2020-11-20 15:08:12 -0500

    Added basic inquirer structure for each command

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # employee-tracker
+
+## Development notes
+
+Departments
+
+- 1 Sales
+- 2 Engineering
+- 3 Finance
+- 4 Legal
+
+Roles
+
+- 1 Sales Lead - 100000 - sales
+- 2 Salesperson - 80000 - sales
+- 3 Lead Engineer - 150000 - engineering
+- 4 Software Engineer - 120000 - engineering
+- 5 Account Manager - 140000 - finance
+- 6 Accountant - 125000 - finance
+- 7 Legal Team Lead - 250000 - legal
+- 8 Lawyer - 190000 - legal
--- employeeTracker.js
+++ employeeTracker.js
@@ -1,3 +1,4 @@
+const inquirer = require("inquirer");
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
@@ -17,5 +18,85 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
+
+  manageEmployees();
   connection.end();
 });
+
+const manageEmployees = () => {
+  inquirer
+    .prompt({
+      name: "selection",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "View All Departments",
+        "View All Roles",
+        "Add Employee",
+        "Add Department",
+        "Add Role",
+        "Update Employee Role",
+      ],
+    })
+    .then((answer) => {
+      switch (answer.selection) {
+        case "View All Employees":
+          viewEmployees();
+          break;
+        case "View All Departments":
+          viewDepartments();
+          break;
+        case "View All Roles":
+          viewRoles();
+          break;
+        case "Add Employee":
+          addEmployee();
+          break;
+        case "Add Department":
+          addDepartment();
+          break;
+        case "Add Role":
+          addRoles();
+          break;
+        case "Update Employee Role":
+          updateEmployeeRole();
+          break;
+      }
+    });
+};
+
+// view employees function
+const viewEmployees = () => {
+  console.log("View employees here");
+};
+
+// view departments function
+const viewDepartments = () => {
+  console.log("View departments here");
+};
+
+// view roles function
+const viewRoles = () => {
+  console.log("View roles here");
+};
+
+// add employee function
+const addEmployee = () => {
+  console.log("add employees here");
+};
+
+// add department function
+const addDepartment = () => {
+  console.log("add department here");
+};
+
+// add role function
+const addRoles = () => {
+  console.log("add roles here");
+};
+
+//update employee roles function
+const updateEmployeeRole = () => {
+  console.log("update employee role here");
+};
